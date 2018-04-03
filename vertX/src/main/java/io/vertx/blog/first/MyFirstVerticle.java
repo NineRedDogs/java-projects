1f46d2609e85cf8493a7252eb901b9be629dd00f
agrahame <andrew@agrahame.com>
2018-04-03 16:46:33 +0100

    added my first vertX project

---
+++ vertX/src/main/java/io/vertx/blog/first/MyFirstVerticle.java
@@ -0,0 +1,31 @@
+package io.vertx.blog.first;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+/**
+ * Taken from : https://vertx.io/blog/my-first-vert-x-3-application/
+ * 
+ * @author agrahame
+ *
+ */
+public class MyFirstVerticle extends AbstractVerticle {
+	
+	  @Override
+	  public void start(Future<Void> fut) {
+	    vertx
+	        .createHttpServer()
+	        .requestHandler(r -> {
+	          r.response().end("<h1>Hello from my first " +
+	              "Vert.x 3 application</h1>");
+	        })
+	        .listen(8080, result -> {
+	          if (result.succeeded()) {
+	            fut.complete();
+	          } else {
+	            fut.fail(result.cause());
+	          }
+	        });
+	  }
+
+}
