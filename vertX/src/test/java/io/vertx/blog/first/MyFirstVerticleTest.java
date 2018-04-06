fabae6d2ce11f4d7b728f9751abfa8b8963b4759
Andy Grahame <agrahame@ns-technologies.com>
2018-04-06 15:44:23 +0100

    half way thru the test part of the tutorial

--- vertX/src/test/java/io/vertx/blog/first/MyFirstVerticleTest.java
+++ vertX/src/test/java/io/vertx/blog/first/MyFirstVerticleTest.java
@@ -2,6 +2,7 @@ package io.vertx.blog.first;
 
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
@@ -44,7 +45,20 @@ public class MyFirstVerticleTest {
   public void tearDown(TestContext context) {
     vertx.close(context.asyncAssertSuccess());
   }
-
+  
+  @Test
+  public void checkThatTheIndexPageIsServed(TestContext context) {
+    Async async = context.async();
+    vertx.createHttpClient().getNow(port, "localhost", "/assets/index.html", response -> {
+      context.assertEquals(response.statusCode(), 200);
+      context.assertEquals(response.headers().get("content-type"), "text/html");
+      response.bodyHandler(body -> {
+        context.assertTrue(body.toString().contains("<title>My Team Collection</title>"));
+        async.complete();
+      });
+    });
+  }
+  
   @Test
   public void testMyApplication(TestContext context) {
     final Async async = context.async();
@@ -57,4 +71,30 @@ public class MyFirstVerticleTest {
       });
     });
   }
+  
+  @Test
+  public void checkThatWeCanAdd(TestContext context) {
+    Async async = context.async();
+    final String json = Json.encodePrettily(new Team("Arsenal", "England"));
+    final String length = Integer.toString(json.length());
+    vertx.createHttpClient().post(port, "localhost", "/api/teams")
+        .putHeader("content-type", "application/json")
+        .putHeader("content-length", length)
+        .handler(response -> {
+          context.assertEquals(response.statusCode(), 201);
+          context.assertTrue(response.headers().get("content-type").contains("application/json"));
+          response.bodyHandler(body -> {
+            final Team newTeam = Json.decodeValue(body.toString(), Team.class);
+            context.assertEquals(newTeam.getName(), "Arsenal");
+            context.assertEquals(newTeam.getCountry(), "England");
+            context.assertNotNull(newTeam.getId());
+            async.complete();
+          });
+        })
+        .write(json)
+        .end();
+  }
+  
+  
+  
 }
