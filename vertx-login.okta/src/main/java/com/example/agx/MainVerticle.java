312b1ec8cd4b371c887dc75742270ba6ed3359d8
Andy Grahame <agrahame@ns-technologies.com>
2018-04-09 11:32:48 +0100

    created 2 login projects

---
+++ vertx-login.okta/src/main/java/com/example/agx/MainVerticle.java
@@ -0,0 +1,111 @@
+package com.example.agx;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.okta.jwt.Jwt;
+import com.okta.jwt.JwtHelper;
+import com.okta.jwt.JwtVerifier;
+
+import io.vertx.config.ConfigRetriever;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.oauth2.OAuth2Auth;
+import io.vertx.ext.auth.oauth2.OAuth2ClientOptions;
+import io.vertx.ext.auth.oauth2.OAuth2FlowType;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.AuthHandler;
+import io.vertx.ext.web.handler.OAuth2AuthHandler;
+
+public class MainVerticle extends AbstractVerticle {
+
+	@Override
+	public void start() throws Exception {
+		ConfigStoreOptions fileStore = new ConfigStoreOptions()
+				.setType("file")
+				.setConfig(new JsonObject().put("path", "src/main/application.json"));
+		ConfigRetrieverOptions options = new ConfigRetrieverOptions()
+				.addStore(fileStore);
+		ConfigRetriever retriever = ConfigRetriever.create(vertx, options);
+		retriever.getConfig(ar -> {
+			if (ar.failed()) {
+				System.err.println("failed to retrieve config.");
+			} else {
+				config().mergeIn(ar.result());
+				startServer();
+			}
+		});
+	}
+
+
+	void startServer() {
+		Router router = Router.router(vertx);
+
+		//create and register the auth handler to intercept all
+		//requests below the /private/ URI:
+		AuthHandler authHandler = getOAuthHandler(router);
+		router.route("/private/*").handler(authHandler);
+
+		router.route("/private/secret")
+		.handler(ctx -> {
+			Map claims = getIdClaims(ctx);
+			ctx.response().end("Hi " +
+                    claims.get("name") +
+                    ", the email address we have on file for you is: "+
+                    claims.get("email"));
+		});
+
+		vertx.createHttpServer()
+		.requestHandler(router::accept)
+		.listen(config().getInteger("port"));
+
+		/**  Router router = Router.router(vertx);
+
+    router.route("/private/secret")
+	    .handler(ctx -> {
+        ctx.response().end("Hi");
+    });
+
+    vertx.createHttpServer()
+		.requestHandler(router::accept)
+		.listen(config().getInteger("port")); */
+	}
+
+	AuthHandler getOAuthHandler(Router router) {
+		OAuth2Auth oauth2 = OAuth2Auth.create(vertx, OAuth2FlowType.AUTH_CODE, new OAuth2ClientOptions()
+				.setClientID(config().getString("clientId"))
+				.setClientSecret(config().getString("clientSecret"))
+				.setSite(config().getString("issuer"))
+				.setTokenPath("/v1/token")
+				.setAuthorizationPath("/v1/authorize")
+				.setUserInfoPath("/v1/userinfo")
+				.setUseBasicAuthorizationHeader(false)
+				);
+
+		OAuth2AuthHandler authHandler = OAuth2AuthHandler.create(oauth2, config().getString("callbackUrl"));
+		authHandler.extraParams(new JsonObject("{\"scope\":\"openid profile email\"}"));
+		authHandler.setupCallback(router.route());
+		return authHandler;
+	}
+
+	
+	Map<String, Object> getIdClaims(RoutingContext ctx) {
+	    try {
+	        JwtVerifier jwtVerifier = new JwtHelper()
+	            .setIssuerUrl(config().getString("issuer"))
+	            .setAudience("api://default")
+	            .setClientId(config().getString("clientId"))
+	            .build();
+
+	        Jwt idTokenJwt = jwtVerifier.decodeIdToken(ctx.user().principal().getString("id_token"), null);
+	        return idTokenJwt.getClaims();
+	    } catch (Exception e) {
+	        //do something with the exception...
+	        return new HashMap<>();
+	    }
+	}
+
+}
