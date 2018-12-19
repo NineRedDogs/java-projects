6dc30c10f6aab14cb0f0be9c3b15d0a515b59140
grahama2 <Andrew.GRAHAME2@admiralgroup.co.uk>
2018-12-19 11:12:52 +0000

    more updates

--- android/FlexiTimer/app/src/main/java/com/example/agrahame/flexitimer/timing/Day.java
+++ android/FlexiTimer/app/src/main/java/com/example/agrahame/flexitimer/timing/Day.java
@@ -1,7 +1,30 @@
 package com.example.agrahame.flexitimer.timing;
 
-public class Day {
+import com.example.agrahame.flexitimer.timing.exceptions.TimeException;
 
+import java.time.LocalDate;
+
+public abstract class Day {
+
+    private TimePair inOut;
+
+    public void clockIn(Time clockInTime) {
+        inOut.setT1(clockInTime);
+    }
+
+    public void clockOut(Time clockOutTime) {
+        inOut.setT2(clockOutTime);
+    }
+
+    public LocalDate getDate() throws TimeException {
+        if (inOut.getT1() == null) {
+            throw new TimeException("No times entered yet, so cannot determine date");
+        }
+        return inOut.getT1().getTime().toLocalDate();
+
+    }
+
+    public abstract void
 
 
 }
