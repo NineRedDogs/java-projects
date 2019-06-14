f6c1a8f941d3e2786f86cb9f747118e8837ad861
NineRedDogs <andrew@agrahame.com>
2019-06-14 11:48:23 +0100

    mods

--- google-home-projects/fnol/functions/index.js
+++ google-home-projects/fnol/functions/index.js
@@ -40,7 +40,7 @@ const app = dialogflow({debug: true});
 // Handle the Dialogflow intent named 'Start Intent'.
 app.intent('start Fnol', (conv) => {
   // uncomment following line to clear stored user info
-  // conv.user.storage = {};
+  conv.user.storage = {};
   const name = conv.user.storage.userName;
   if (!name) {
     // Asks the user's permission to know their name, for personalization.
@@ -67,14 +67,14 @@ app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
   //conv.ask(`${fnolQueryPrefix}, What seems to be the problem?`);
   //conv.ask(new Suggestions('Leaky roof', 'Fence Blown down', 'a Break-in'));
   if (!permissionGranted) {
-    conv.ask(`Ok, no worries. 11:41  What's the fnol problem ?`);
+    conv.ask(`Ok, no worries. 11:48  What's the fnol problem ?`);
     conv.ask(new Suggestions('Leaky roof', 'Fence Blown down', 'a Break-in'));
   } else {
     //conv.data.userName = conv.user.name.display;
 	  //conv.user.storage.userName = conv.user.name.given;
     conv.user.storage.userName = conv.user.name.given;
   }
-  conv.ask(`Thanks, ${conv.user.storage.userName}. 11:41  What's the fnol problem ?`);
+  conv.ask(`Thanks, ${conv.user.storage.userName}. 11:48  What's the fnol problem ?`);
   //conv.ask(`Thanks, ${conv.data.userName}. 11:36  What's the fnol problem ?`);
   conv.ask(new Suggestions('Leaky roof', 'Fence Blown down', 'a Break-in'));
 });
