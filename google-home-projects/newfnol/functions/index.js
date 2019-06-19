c5e8b903791700d84d730d2cdf8120d1842e1f43
NineRedDogs <andrew@agrahame.com>
2019-06-19 15:49:59 +0100

    mods

--- google-home-projects/newfnol/functions/index.js
+++ google-home-projects/newfnol/functions/index.js
@@ -35,7 +35,8 @@ const functions = require('firebase-functions');
 
 const url = require('url');
 //const {ssml} = require('./util');
-const { ssml } = require('actions-on-google/util/ssml');
+//const { ssml } = require('actions-on-google/util/ssml');
+const { ssml } = require('./util/ssml');
 
 const config = functions.config();
 const STATIC_MAPS_ADDRESS = 'https://maps.googleapis.com/maps/api/staticmap';
