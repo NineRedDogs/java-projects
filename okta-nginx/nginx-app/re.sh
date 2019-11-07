6dbebc8dc3147bf9a7dfb8acabc3344848af33df
NineRedDogs <andrew@agrahame.com>
2019-11-07 15:45:47 +0000

    more refactoring

--- okta-nginx/nginx-app/re.sh
+++ okta-nginx/nginx-app/re.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-echo " "; echo " -- --  -"
-docker rm -f nginxplus
+# script to start up nginx-oidc environment
+
+# constants
+APP_DIR=$(pwd)
+
+sh app.sh
+sh nginx.sh
+
+# make surw we end up back in the start dir
+cd $APP_DIR
 
 echo " "; echo " -- --  -"
-docker build --no-cache -t ajg-nginxplus-centos .
-
 echo " "; echo " -- --  -"
-docker run --name nginxplus -p 8000:80 -p 8010:8010 -d ajg-nginxplus-centos
-
-
 echo " "; echo " -- --  -"
-echo "to connect :           $ docker exec -it nginxplus /bin/bash"
+echo "nginx:"
+echo "   to connect :           $ docker exec -it nginxplus /bin/bash"
+echo "   for logs   :           $ docker logs -f nginxplus"
+echo " "; echo " -- --  -"
+echo "spring app:"
+echo "   to connect :           $ docker exec -it nginx-spring-app /bin/bash"
+echo "   for logs   :           $ docker logs -f nginx-spring-app"
 echo " "; echo " -- --  -"
