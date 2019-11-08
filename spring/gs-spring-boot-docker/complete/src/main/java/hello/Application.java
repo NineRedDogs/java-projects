30ddb3eed4b5efd4a9b2818272f782e0d626e569
NineRedDogs <andrew@agrahame.com>
2019-11-08 16:54:34 +0000

    mods

--- spring/gs-spring-boot-docker/complete/src/main/java/hello/Application.java
+++ spring/gs-spring-boot-docker/complete/src/main/java/hello/Application.java
@@ -14,6 +14,11 @@ public class Application {
         return "Hello Docker World";
     }
 
+    @RequestMapping("/xyz")
+    public String xyz() {
+        return "Hello XYZ ....";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
