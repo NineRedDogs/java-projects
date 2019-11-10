0ded9617617a6d2190d99d0aefc2afdf71d1bdf0
Andrew <andrew@andrews-imac.home>
2019-11-10 15:22:30 +0000

    fixes

--- okta-nginx/nginx-app/src/main/java/com/okta/examples/originexample/config/RequestContextUser.java
+++ okta-nginx/nginx-app/src/main/java/com/okta/examples/originexample/config/RequestContextUser.java
@@ -1,18 +1,11 @@
 package com.okta.examples.originexample.config;
 
-import java.time.Duration;
 import java.util.Arrays;
-import java.util.Map;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.okta.examples.originexample.model.User;
-import com.okta.jwt.AccessTokenVerifier;
-import com.okta.jwt.Jwt;
-import com.okta.jwt.JwtVerificationException;
-import com.okta.jwt.JwtVerifiers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,32 +16,30 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 public class RequestContextUser {
 
     private static final Logger log = LoggerFactory.getLogger(RequestContextUser.class);
-    private static final ObjectMapper mapper = new ObjectMapper();
 
-    public static final String USER_HEADER = "x-userinfo";
-    public static final String AUTH_HDR = "authorization";
-    public static final String USERNAME_HDR = "username";
+    public static final String HDR_EMAIL = "adm-email";
+    public static final String HDR_GROUP = "adm-group";
+    public static final String HDR_USERNAME = "adm-username";
+    public static final String HDR_USER_ID = "adm-sub";
 
     public static User findUser() {
-        String userInfoHeader;
         HttpServletRequest req;
-
         log.error("in findUser ..........................");
 
         try {
             RequestAttributes reqAttr = RequestContextHolder.currentRequestAttributes();
 
-            int scope = RequestAttributes.SCOPE_REQUEST;
+            // int scope = RequestAttributes.SCOPE_REQUEST;
+            // String[] aNames = reqAttr.getAttributeNames(scope);
+            // for (String name : aNames) {
+            // StringBuilder sb = new StringBuilder("SCOPE: REQUEST::: attr: ");
+            // sb.append(name);
+            // sb.append(" val: ");
+            // sb.append(reqAttr.getAttribute(name, scope));
+            // log.error(sb.toString());
+            // }
+            int scope = RequestAttributes.SCOPE_SESSION;
             String[] aNames = reqAttr.getAttributeNames(scope);
-            for (String name : aNames) {
-                StringBuilder sb = new StringBuilder("SCOPE: REQUEST::: attr: ");
-                sb.append(name);
-                sb.append(" val: ");
-                sb.append(reqAttr.getAttribute(name, scope));
-                log.error(sb.toString());
-            }
-            scope = RequestAttributes.SCOPE_SESSION;
-            aNames = reqAttr.getAttributeNames(scope);
             for (String name : aNames) {
                 StringBuilder sb = new StringBuilder("SCOPE: SESSION::: attr: ");
                 sb.append(name);
@@ -57,112 +48,29 @@ public class RequestContextUser {
                 log.error(sb.toString());
             }
 
-            log.error("reqAttr class : " + reqAttr.getClass().getName());
-            log.error("reqAttr getReq : " + ((ServletRequestAttributes) reqAttr).getRequest());
-            log.error("user header : " + ((ServletRequestAttributes) reqAttr).getRequest().getHeader(USER_HEADER));
-            final String authHdr = ((ServletRequestAttributes) reqAttr).getRequest().getHeader(AUTH_HDR);
-            log.error("auth header : " + ((ServletRequestAttributes) reqAttr).getRequest().getHeader(AUTH_HDR));
+            String username = ((ServletRequestAttributes) reqAttr).getRequest().getHeader(HDR_USERNAME);
+            String groups = ((ServletRequestAttributes) reqAttr).getRequest().getHeader(HDR_GROUP);
+            String email = ((ServletRequestAttributes) reqAttr).getRequest().getHeader(HDR_EMAIL);
+            String userId = ((ServletRequestAttributes) reqAttr).getRequest().getHeader(HDR_USER_ID);
 
-            final String usernameHdr = ((ServletRequestAttributes) reqAttr).getRequest().getHeader(USERNAME_HDR);
-            log.error("username header : " + ((ServletRequestAttributes) reqAttr).getRequest().getHeader(USERNAME_HDR));
+            log.error("username : " + username);
+            log.error("groups : " + groups);
+            log.error("email : " + email);
+            log.error("user id : " + userId);
 
-            User user2FromAuth = decodeAndExtractUsername(usernameHdr);
+            List<String> groupArray = Arrays.asList(groups.split(","));
 
+            User user = new User(email, username, userId, groupArray);
 
-            User userFromAuth = decodeAndExtractUserData(authHdr);
-            if (userFromAuth != null) {
+            if (user != null) {
                 req = ((ServletRequestAttributes) reqAttr).getRequest();
-                req.setAttribute(User.class.getName(), userFromAuth);
-                return userFromAuth;
+                req.setAttribute(User.class.getName(), user);
+                return user;
             }
         } catch (Exception e) {
-            log.error("Unable to resolve user from {} header", USER_HEADER, e);
+            log.error("Unable to resolve user", e);
         }
-        log.debug("Did not find user from {} header.", USER_HEADER);
+        log.debug("Did not find user info");
         return null;
     }
-
-    private static User decodeAndExtractUserData(String authHdr) {
-        log.error("[[[ in decodeAndExtractUserData ..... ]]]]");
-
-        // 1. strip bearer text
-        final String bearer = "Bearer ";
-        // final String clientSecret = "XaewflSG_nlmMm07971BHd69Qj-fTrqdQXtN2xwd";
-        log.error("Auth hdr : " + authHdr);
-        // Bearer eyJraWQiOiJOdFl5al9uYkhUREdxOVo2c2dVaS1mS
-
-        if (authHdr != null && authHdr.startsWith(bearer)) {
-            final String strippedAuthHdr = authHdr.substring(bearer.length());
-            log.error("(Stripped) Auth hdr : " + strippedAuthHdr);
-
-            // 2. decode and create user object
-            User user = decodeJwt(strippedAuthHdr);
-            return user;
-        }
-        return null;
-    }
-
-    private static User decodeAndExtractUsername(String unHdr) {
-        log.error("[[[ in decodeAndExtractUsername ..... ]]]]");
-
-        // final String clientSecret = "XaewflSG_nlmMm07971BHd69Qj-fTrqdQXtN2xwd";
-        log.error("username hdr : " + unHdr);
-
-        if (unHdr != null) {
-
-            // 1. decode and create user object
-            User user = decodeJwt(unHdr);
-            return user;
-        }
-        return null;
-    }
-
-    public static User decodeJwt(String jwtEncoded) {
-        AccessTokenVerifier jwtVerifier = JwtVerifiers.accessTokenVerifierBuilder()
-                .setIssuer("https://dev-424995.okta.com/oauth2/default").setAudience("api://default") // defaults to
-                                                                                                      // 'api://default'
-                .setConnectionTimeout(Duration.ofSeconds(1)) // defaults to 1s
-                .setReadTimeout(Duration.ofSeconds(1)) // defaults to 1s
-                .build();
-
-        try {
-            log.error("AJG=1==========================");
-            Jwt jwt = jwtVerifier.decode(jwtEncoded);
-            Map<String, Object> claims = jwt.getClaims();
-            log.error(Arrays.toString(claims.entrySet().toArray()));
-            log.error("AJG=2==========================");
-
-            final String keyID = "sub";
-            final String keyFirstName = "user.firstName";
-            final String keyLastName = "user.lastName";
-            final String keyEmail = "user.email";
-            final String keyGroups = "groups";
-
-            String id = (String) claims.get(keyID);
-            log.error("ID : " + id);
-
-            String firstName = (String) claims.get(keyFirstName);
-            String lastName = (String) claims.get(keyLastName);
-            log.error("Full name : [" + firstName + "_" + lastName + "]");
-
-            String email = (String) claims.get(keyEmail);
-            log.error("email : " + email);
-
-            List<String> groups = (List<String>) claims.get(keyGroups);
-            StringBuilder sb = new StringBuilder("Groups: [");
-            for (String g : groups) {
-                sb.append(g + ",");
-            }
-            sb.append("] numGroups : " + groups.size());
-
-            log.error("groups : " + sb.toString());
-
-            User u = new User(email, firstName + " " + lastName, id, groups);
-            return u;
-        } catch (JwtVerificationException e) {
-            log.error("Caught JwtVerificationException when decoding jwt, e:" + e.getMessage());
-        }
-        return null;
-    }
-
 }
