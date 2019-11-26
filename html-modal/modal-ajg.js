7c4c9d4cf118d588cb467e74d8b34a4aa53bab2a
NineRedDogs <andrew@agrahame.com>
2019-11-26 15:53:40 +0000

    added modal proj

---
+++ html-modal/modal-ajg.js
@@ -0,0 +1,34 @@
+//Get modal element
+var modal = document.getElementById('simpleModal');
+//Get modal button
+var modalBtn = document.getElementById('modalBtn');
+//Get close button
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', clickOutside);
+
+// function to open modal
+function openModal(){
+    console.log("ajg pressed open button");
+    modal.style.display = 'block';
+}
+
+// function to open modal
+function closeModal(){
+    console.log("ajg pressed closed button");
+    modal.style.display = 'none';
+}
+
+// function to open modal
+function clickOutside(e){
+    console.log("ajg pressed click outside");
+    if(e.target == modal) {
+       modal.style.display = 'none';
+    }
+}
