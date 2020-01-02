b8752abea6b93fa1e0e7e914414ea821e08d837b
Andrew <andrew@andrews-imac.home>
2020-01-02 16:17:06 +0000

    new project in game-oracle to collect results

---
+++ ajg-java/GameOracle/gatherer/data/sample-data-20-dec-2019/getResultsFiles.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# england
+wget http://www.football-data.co.uk/mmz4281/1920/E0.csv
+wget http://www.football-data.co.uk/mmz4281/1920/E1.csv
+wget http://www.football-data.co.uk/mmz4281/1920/E2.csv
+wget http://www.football-data.co.uk/mmz4281/1920/E3.csv
+wget http://www.football-data.co.uk/mmz4281/1920/EC.csv
+
+# scotland
+wget http://www.football-data.co.uk/mmz4281/1920/SC0.csv
+wget http://www.football-data.co.uk/mmz4281/1920/SC1.csv
+wget http://www.football-data.co.uk/mmz4281/1920/SC2.csv
+wget http://www.football-data.co.uk/mmz4281/1920/SC3.csv
+
+# germany
+wget http://www.football-data.co.uk/mmz4281/1920/D1.csv
+
+# italy
+wget http://www.football-data.co.uk/mmz4281/1920/I1.csv
+
+# spain
+wget http://www.football-data.co.uk/mmz4281/1920/SP1.csv
+
+# france
+wget http://www.football-data.co.uk/mmz4281/1920/F1.csv
+
+# netherlands
+wget http://www.football-data.co.uk/mmz4281/1920/N1.csv
+
+# belgium
+wget http://www.football-data.co.uk/mmz4281/1920/B1.csv
+
+# portugal
+wget http://www.football-data.co.uk/mmz4281/1920/P1.csv
+
+
