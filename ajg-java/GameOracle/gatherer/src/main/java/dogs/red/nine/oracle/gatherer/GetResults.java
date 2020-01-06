d290871a99e4d9d2afc1a10c7170c2705da87fcd
Andy Grahame <andrew.grahame2@admiralgroup.co.uk>
2020-01-06 15:52:37 +0000

    mods

--- ajg-java/GameOracle/gatherer/src/main/java/dogs/red/nine/oracle/gatherer/GetResults.java
+++ ajg-java/GameOracle/gatherer/src/main/java/dogs/red/nine/oracle/gatherer/GetResults.java
@@ -2,7 +2,9 @@ package dogs.red.nine.oracle.gatherer;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -10,11 +12,14 @@ import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import dogs.red.nine.oracle.data.Division;
 import dogs.red.nine.oracle.data.MatchData;
@@ -58,12 +63,21 @@ public class GetResults {
 		return dataUrls;
 	}
 
-	private void getResultsDataFilesFromRemote() {
-		FileUtils.copyURLToFile(
-  new URL("http://example.com/my-file-path.txt"), 
-  new File("/Users/username/Documents/file_name.txt"), 
-  CONNECTION_TIMEOUT, 
-  READ_TIMEOUT);
+	private File getResultsDataFilesFromRemote(final String datafileUrl) {
+
+		final String fname = FilenameUtils.getName(datafileUrl);
+		final File localFile = new File(Gatherer.DATA_DIR, fname);
+
+		try {
+			InputStream in = new URL(datafileUrl).openStream();
+			Files.copy(in, Paths.get(localFile.toURI()), StandardCopyOption.REPLACE_EXISTING);
+			logger.debug("set up local file : " + fname);
+		}
+		catch (Exception ex) {
+			logger.debug("using existing data file (if one exists), as couldnt get file [" + datafileUrl + "] ex: " + ex.getClass().getName() + ", " + ex.getLocalizedMessage());
+		}
+
+		return localFile;
 	}
 
 	
@@ -75,21 +89,25 @@ public class GetResults {
 			final String divisionResultsUrl = ResultDataUrlUtils.generateResultUrl(division);
 
 			logger.debug("getting results from data url : " + divisionResultsUrl);
+
+			final File dataFile = getResultsDataFilesFromRemote(divisionResultsUrl);
+
 			String[] keyData=null;
 			
-			URL url = null;
+			// URL url = null;
 			String lineReadFromDataFile;
 			
-			try {
-			    url = new URL(divisionResultsUrl);
-			} catch (MalformedURLException e) {
-			    e.printStackTrace();
-			}
+			// try {
+			//     url = new URL(divisionResultsUrl);
+			// } catch (MalformedURLException e) {
+			//     e.printStackTrace();
+			// }
 			BufferedReader in;
 			try {
-			    URLConnection con = url.openConnection();
-			    con.setReadTimeout( 1000 ); //1 second
-			    in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			    // URLConnection con = url.openConnection();
+			    // con.setReadTimeout( 1000 ); //1 second
+			    // in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			    in = new BufferedReader(new InputStreamReader(new FileInputStream(dataFile)));
 			    while ((lineReadFromDataFile = in.readLine()) != null) {
 			    	if (lineReadFromDataFile.startsWith(DATA_FILE_COLUMN_KEY_LINE)) {
 			    		String keyLine = lineReadFromDataFile;
