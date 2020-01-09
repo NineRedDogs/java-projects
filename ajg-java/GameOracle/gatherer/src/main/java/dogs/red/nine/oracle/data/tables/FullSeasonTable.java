90f3b6cf10da9cb33a788d85e1be7679485b723c
Andy Grahame <andrew.grahame2@admiralgroup.co.uk>
2020-01-09 15:52:21 +0000

    tables

--- ajg-java/GameOracle/gatherer/src/main/java/dogs/red/nine/oracle/data/tables/FullSeasonTable.java
+++ ajg-java/GameOracle/gatherer/src/main/java/dogs/red/nine/oracle/data/tables/FullSeasonTable.java
@@ -1,7 +1,21 @@
 package dogs.red.nine.oracle.data.tables;
 
+import dogs.red.nine.oracle.data.Division;
+import dogs.red.nine.oracle.data.MatchData;
+
+import java.util.List;
+import java.util.SortedSet;
+
 public class FullSeasonTable extends Table {
-    public FullSeasonTable() {
-        super(tableName);
+    protected FullSeasonTable(Division division, SortedSet<String> teams) {
+        super("Full Table", division, teams);
+    }
+
+    public void generateTable(List<MatchData> matches) {
+
+        for (MatchData match : matches) {
+
+        }
+
     }
 }
