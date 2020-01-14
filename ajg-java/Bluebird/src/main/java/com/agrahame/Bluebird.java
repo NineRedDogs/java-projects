1a32ef9c19aa918083e3fbb8bf77315abdb5dc4e
NineRedDogs <andrew@agrahame.com>
2020-01-14 15:05:55 +0000

    New project

---
+++ ajg-java/Bluebird/src/main/java/com/agrahame/Bluebird.java
@@ -0,0 +1,13 @@
+package com.agrahame;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Bluebird {
+
+    private static final Logger logger = LogManager.getLogger("Bluebird");
+
+    public static void main(String[] args) {
+        logger.debug("we are bluebird !!");
+    }
+}
