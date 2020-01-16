c3b9740e4019b875110734ee319312c9ffb1b23e
Andrew <andrew@andrews-imac.home>
2020-01-16 22:14:40 +0000

    added home and away tables

--- ajg-java/Bluebird/src/main/java/dogs/red/nine/oracle/data/tables/FullSeasonAwayTable.java
+++ ajg-java/Bluebird/src/main/java/dogs/red/nine/oracle/data/tables/FullSeasonAwayTable.java
@@ -5,23 +5,22 @@ import dogs.red.nine.oracle.data.MatchData;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
+import java.util.SortedSet;
 
-public class FullSeasonTable extends Table {
-    private static final Logger logger = LogManager.getLogger("FullSeasonTable");
+public class FullSeasonAwayTable extends Table {
+    private static final Logger logger = LogManager.getLogger("FullSeasonAwayTable");
 
-    protected FullSeasonTable(Division division, SortedSet<String> teams) {
-        super("Full Table", division, teams);
+    protected FullSeasonAwayTable(Division division, SortedSet<String> teams) {
+        super("Full Away Table", division, teams);
         for (String team : teams) {
-            table.put(team, new FullSeasonTableEntry(team));
+            table.put(team, new FullSeasonAwayTableEntry(team));
         }
     }
 
     public void generateTable(List<MatchData> matches) {
 
         for (MatchData match : matches) {
-            table.get(match.getHomeTeam()).addResult(match);
             table.get(match.getAwayTeam()).addResult(match);
         }
         table = sortTable();
---
+++ ajg-java/Bluebird/src/main/java/dogs/red/nine/oracle/data/tables/FullSeasonHomeTableEntry.java
@@ -0,0 +1,42 @@
+package dogs.red.nine.oracle.data.tables;
+
+import dogs.red.nine.oracle.data.MatchData;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class FullSeasonHomeTableEntry extends TableEntry {
+
+    private static final Logger logger = LogManager.getLogger("FullSeasonHomeTableEntry");
+
+
+    public FullSeasonHomeTableEntry(String teamName) {
+        super(teamName);
+    }
+
+    public void addResult(final MatchData result) {
+        if (getTeamName().equalsIgnoreCase(result.getHomeTeam())) {
+            // team is home team
+            add(result.getHomeTeamScore(), result.getAwayTeamScore());
+
+        } else {
+            logger.debug("current team not home in given match !!!");
+        }
+    }
+
+    private void add(int ourScore, int otherTeamScore) {
+        incrementGamesPlayed();
+        if (ourScore > otherTeamScore) {
+            incrementGamesWon();
+            incrementPoints(3);
+        } else if (ourScore == otherTeamScore) {
+            incrementGamesDrawn();
+            incrementPoints(1);
+        } else {
+            incrementGamesLost();
+        }
+        incrementGoalsFor(ourScore);
+        incrementGoalsAgainst(otherTeamScore);
+
+    }
+
+}
