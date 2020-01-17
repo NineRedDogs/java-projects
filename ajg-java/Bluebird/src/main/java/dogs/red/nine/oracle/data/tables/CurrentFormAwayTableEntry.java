c90a6559163420026285accfaae31e5d75f213cd
Andy Grahame <andrew.grahame2@admiralgroup.co.uk>
2020-01-17 14:48:13 +0000

    added form tables

--- ajg-java/Bluebird/src/main/java/dogs/red/nine/oracle/data/tables/CurrentFormAwayTableEntry.java
+++ ajg-java/Bluebird/src/main/java/dogs/red/nine/oracle/data/tables/CurrentFormAwayTableEntry.java
@@ -4,25 +4,21 @@ import dogs.red.nine.oracle.data.MatchData;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class CurrentFormTableEntry extends TableEntry {
+public class CurrentFormAwayTableEntry extends TableEntry {
 
-    private static final Logger logger = LogManager.getLogger("CurrentFormTableEntry");
+    private static final Logger logger = LogManager.getLogger("CurrentFormAwayTableEntry");
 
 
-    public CurrentFormTableEntry(String teamName) {
+    public CurrentFormAwayTableEntry(String teamName) {
         super(teamName);
     }
 
     public void addResult(final MatchData result) {
-        if (getTeamName().equalsIgnoreCase(result.getHomeTeam())) {
-            // team is home team
-            add(result.getHomeTeamScore(), result.getAwayTeamScore());
-
-        } else if (getTeamName().equalsIgnoreCase(result.getAwayTeam())) {
+        if (getTeamName().equalsIgnoreCase(result.getAwayTeam())) {
             // team is away team
             add(result.getAwayTeamScore(), result.getHomeTeamScore());
         } else {
-            logger.debug("current team not home OR away in given match !!!");
+            logger.debug("current team not away in given match !!!");
         }
     }
 
