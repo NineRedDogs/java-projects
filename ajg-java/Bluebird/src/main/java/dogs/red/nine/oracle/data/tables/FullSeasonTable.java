e72487bb34bcd78aef81873a4b396e2ba944e73f
andrew <andrew@agrahame.com>
2020-05-15 19:06:18 +0100

    Added picocli CLI options support. Can now provide season, number of current form games, leagues to process and the 'use only todays games' flag

--- ajg-java/Bluebird/src/main/java/dogs/red/nine/oracle/data/tables/FullSeasonTable.java
+++ ajg-java/Bluebird/src/main/java/dogs/red/nine/oracle/data/tables/FullSeasonTable.java
@@ -1,18 +1,19 @@
 package dogs.red.nine.oracle.data.tables;
 
+import dogs.red.nine.oracle.Config;
 import dogs.red.nine.oracle.data.Division;
 import dogs.red.nine.oracle.data.MatchData;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
+import java.util.SortedSet;
 
 public class FullSeasonTable extends Table {
     private static final Logger logger = LogManager.getLogger("FullSeasonTable");
 
-    protected FullSeasonTable(Division division, SortedSet<String> teams) {
-        super("Full Table", division, teams);
+    protected FullSeasonTable(Division division, Config cfg, SortedSet<String> teams) {
+        super("Full Table", division, cfg, teams);
         for (String team : teams) {
             addEntry(team, new FullSeasonTableEntry(team));
         }
